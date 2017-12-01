429534ab3e15d5d971a4c97efb3c89416deb0399
buckethead1986 <rspeckpfs@gmail.com>
2017-12-01 16:30:02 -0500

    Challenge fetch working

--- app/controllers/api/v1/challenges_controller.rb
+++ app/controllers/api/v1/challenges_controller.rb
@@ -8,4 +8,19 @@ class Api::V1::ChallengesController < ActionController::API
     @challenge = Challenge.find_by(id: params[:id])
     render json: @challenge.to_json(:include => {:user_challenges => {:include => :user }})
   end
+
+  def new
+
+  end
+
+  def create
+    @challenge = Challenge.create(challenge_params)
+
+  end
+
+  private
+
+  def challenge_params
+    params.require(:challenge).permit(:name, :description, :challenge_type, :criteria, :team_names)
+  end
 end
