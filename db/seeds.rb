c44049d74014577fda2e08fb003275ee67cb96c0
Nick Paolino <github email address>
2017-12-01 11:07:25 -0500

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: "Nick", password: "abc")
+User.create(username: "Robert", password: "abc")
+User.create(username: "Ramy", password: "abc")
+
+Challenge.create(
+  name: "Blogging in Style",
+  description: "Who can wear the most formal outfit to blogs?",
+  public: true,
+  team_names: "The Avengers/The Incredibles"
+)
+
+UserChallenge.create(
+  user_id: 1,
+  challenge_id: 1,
+  role: "1"
+)
+
+UserChallenge.create(
+  user_id: 2,
+  challenge_id: 1,
+  role: "2"
+)
+
+UserChallenge.create(
+  user_id: 3,
+  challenge_id: 1,
+  role: "3"
+)
