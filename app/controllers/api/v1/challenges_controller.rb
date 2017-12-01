6c4c51859fc6e32016ac907f94090fbea827fc1c
Nick Paolino <github email address>
2017-12-01 10:54:32 -0500

    Added challenge show controller action for json

--- app/controllers/api/v1/challenges_controller.rb
+++ app/controllers/api/v1/challenges_controller.rb
@@ -3,4 +3,9 @@ class Api::V1::ChallengesController < ActionController::API
     @challenges = Challenge.all
     render json: @challenges.to_json(:include => {:user_challenges => {:include => :user }})
   end
+
+  def show
+    @challenge = Challenge.find_by(id: params[:id])
+    render json: @challenge.to_json(:include => {:user_challenges => {:include => :user }})
+  end
 end
