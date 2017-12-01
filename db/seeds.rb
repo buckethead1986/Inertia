d017c823d9017299c5d77a83fb19b7e5fea3b9e4
Nick Paolino <github email address>
2017-12-01 11:19:26 -0500

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,36 @@
-User.create(username: "Nick", password: "abc")
-User.create(username: "Robert", password: "abc")
-User.create(username: "Ramy", password: "abc")
+nick = User.create(username: "Nick", password: "abc")
+robert = User.create(username: "Robert", password: "abc")
+ramy = User.create(username: "Ramy", password: "abc")
 
-Challenge.create(
+first_challenge = Challenge.create(
   name: "Blogging in Style",
   description: "Who can wear the most formal outfit to blogs?",
   public: true,
   team_names: "The Avengers/The Incredibles"
 )
 
-UserChallenge.create(
-  user_id: 1,
-  challenge_id: 1,
-  role: "1"
-)
+user_challenge_one = UserChallenge.new()
 
-UserChallenge.create(
-  user_id: 2,
-  challenge_id: 1,
-  role: "2"
-)
+user_challenge_one.role = "1"
+user_challenge_one.user = nick
+user_challenge_one.challenge_id = first_challenge.id
 
-UserChallenge.create(
-  user_id: 3,
-  challenge_id: 1,
-  role: "3"
-)
+user_challenge_one.save
+
+user_challenge_two = UserChallenge.new()
+
+user_challenge_two.role = "2"
+user_challenge_two.user = robert
+user_challenge_two.challenge_id = first_challenge.id
+
+user_challenge_two.save
+
+user_challenge_three = UserChallenge.new()
+
+user_challenge_three.role = "3"
+user_challenge_three.user = ramy
+user_challenge_three.challenge_id = first_challenge.id
+
+user_challenge_three.save
+
+first_challenge.save
