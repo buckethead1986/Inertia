c69efa3d7a036aae93fecca310b4d71ee3170248
Nick Paolino <github email address>
2017-12-05 13:46:33 -0500

    Added user validations

--- app/controllers/api/v1/user_challenges_controller.rb
+++ app/controllers/api/v1/user_challenges_controller.rb
@@ -9,10 +9,19 @@ class Api::V1::UserChallengesController < ActionController::API
     render json: @user_challenge.to_json
   end
 
+  def update
+    @user = UserChallenge.find_by(id: params[:id])
+    @user.update(vote_params)
+    @user.save
+  end
 
   private
 
   def user_challenge_params
     params.require(:user_challenge).permit(:user_id, :challenge_id, :role)
+  end
+
+  def vote_params
+    params.require(:user_challenge).permit(:vote)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   has_many :user_challenges
   has_many :challenges, through: :user_challenges
   has_many :comments
+
+  validates :username, presence: true
+  validates :username, uniqueness: true
+  validates :username, length: { maximum: 100 }
 end
